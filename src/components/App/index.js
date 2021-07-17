bb6b5a977aa3a88c47607b1d0dd38670aece38dc
AliamineYoussouf <aliamine.youssouf212@gmail.com>
2021-07-18 00:38:46 +0200

    SignUp component creation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,17 @@
 // == Import npm
 import React from 'react';
+import SignUp from '../SignUp';
+// import Home from '../Home';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+
+import './app.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+<SignUp />
+    
   </div>
 );
 
