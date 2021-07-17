5368e5a0451d9fc3123f978d35523ec91e20f3b3
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-07-17 20:45:13 +0200

    delete beginning of the research form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 
 import Header from 'src/components/Header';
-import Research from 'src/components/Research';
 import Footer from 'src/components/Footer';
 
 // == Import
@@ -12,7 +11,6 @@ import './app.scss';
 const App = () => (
   <div className="app">
     <Header />
-    <Research />
     <Footer />
   </div>
 );
