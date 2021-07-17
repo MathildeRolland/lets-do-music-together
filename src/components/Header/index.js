4f45fb681efd35c8497e4703b2b50f001980cd58
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-07-17 21:27:43 +0200

    finalize the repair of the menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ import './header.scss';
 
 const Header = () => (
   <div className="header">
-    <div className="header__test">
+    <div className="header__container">
       <div className="header__logo-container">
           <img className="header__logo" src={logo} />
       </div>
