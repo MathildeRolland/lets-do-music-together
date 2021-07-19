b975d74174139412d8f2f1c04cf18b2a10ffe53e
AliamineYoussouf <aliamine.youssouf212@gmail.com>
2021-07-20 00:25:56 +0200

    modal component css finalization

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,18 +4,20 @@ import React from 'react';
 import './modal.scss';
 
 const Modal = () => (
-  <div className="modal">
-   <div className="modal__header">
-     <h3>Se connecter</h3>
-     <button className="close" type="button">X</button>
+  <div className="modal__background">
+    <div className="modal">
+    <div className="modal__header">
+      <h3>Se connecter</h3>
+      <button className="close" type="button">X</button>
+      </div>
+      <div className="modal__content"> 
+      <form className="modal__form">
+          <input type="email" name="email" id="email" required placeholder="Adresse Email" />
+          <input type="password" name="firstname" id="firstname" required placeholder="Mot de passe" />
+          <input type="submit" className="modal__submit" value="Se connecter" />
+        </form>
     </div>
-    <div className="modal__content"> 
-     <form className="modal__form">
-        <input type="email" name="email" id="email" required placeholder="Adresse Email" />
-        <input type="password" name="firstname" id="firstname" required placeholder="Mot de passe" />
-        <input type="submit" value="Se connecter" />
-      </form>
-   </div>
+    </div>
   </div>
 );
 
