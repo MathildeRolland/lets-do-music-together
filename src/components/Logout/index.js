10541eb734875a9126f0f8696be461d39230d405
AliamineYoussouf <aliamine.youssouf212@gmail.com>
2021-07-28 13:59:54 +0200

    option added to logout modal to allow user to interrupt logout

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -12,15 +12,19 @@ const Logout = ({ manageDeconnexion }) => {
         history.push("/");
     };
 
+    const handleClick = () => {
+      history.goBack();
+    }
+
     return (
         <div className="logout modal__background">
             <div className="modal">
                 <div className="modal__header">
                 <h3>Voulez-vous vous déconnecter?</h3>
-                <button className="close" type="button">X</button>
+                <button className="close" type="button" onClick={handleClick}>X</button>
                 </div>
                 <div className="modal__content"> 
-                    <button type="button" className="modal__submit" value="non">Non, jme suis trompé</button>
+                    <button type="button" className="modal__submit" value="non" onClick={handleClick}>Non, jme suis trompé</button>
                     <button type="button" className="modal__submit" value="oui" onClick={handleDeconnexion}>Oui</button>
                 </div>
             </div>
